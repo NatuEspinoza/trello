f16ca22f7c11ff7a8e701612c61f38901f0cf780
NatuEspinoza <natalia.utemdcv@gmail.com>
2017-11-06 23:21:42 -0300

    Maqueta esqueleto html y css

---
+++ app.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  onClick.onsubmit = function () {
+    var onClick = document.getElementById('onClick');
+    var input = document.createElement('input');
+    var buttom = document.createElement('buttom');
+    var textButtom =
+    onClick.appendChild(input);
+    onClick.appendChild(buttom);
+  }
+}
