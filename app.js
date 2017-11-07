319c71ff2bc4a2ab8d3b55622c229b30f6f6bc89
NatuEspinoza <natalia.utemdcv@gmail.com>
2017-11-07 12:22:59 -0300

    Agregando formulario para agregar lista

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 window.onload = function() {
-  onClick.onsubmit = function () {
-    var onClick = document.getElementById('onClick');
+    //agregando formulario
+    var form = document.getElementById('form');
+    var addList = document.getElementById('addList');
     var input = document.createElement('input');
     var buttom = document.createElement('buttom');
-    var textButtom =
-    onClick.appendChild(input);
-    onClick.appendChild(buttom);
+    var textButtom = document.createTextNode('Guardar');
+    buttom.appendChild(textButtom);
+    var closeButtom = document.createElement('buttom');
+    var textCloseButtom = document.createTextNode('X');
+    closeButtom.appendChild(textCloseButtom);
+    //función que me despliega el formulario
+    addList.onclick = function () {
+      form.removeChild(addList);
+      form.className = 'newForm';
+      form.appendChild(input).className = 'newInput';
+      input.setAttribute("placeholder","Añadir una lista...")
+      form.appendChild(buttom).className = 'btn';
+      form.appendChild(closeButtom).className = 'btn-close';
+    }
   }
-}
